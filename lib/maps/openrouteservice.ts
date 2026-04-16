6058286f17fcac383d765dd0198fd8d0c73cf2ad
Daniel Quah <danielquah148@yahoo.com>
2026-04-16 11:18:32 +1000

    Remove caching

--- lib/maps/openrouteservice.ts
+++ lib/maps/openrouteservice.ts
@@ -5,8 +5,6 @@ const ORS_SERVER = process.env.NEXT_PUBLIC_ORS_SERVER
 
 // TODO: self hosted ORS provide no auth out of the box. Have to figure out auth later on... 
 export async function getRoute(profile: string, coords: [number, number][]){
-    'use cache'
-    cacheLife('days')
     const ors = new OpenRouteService({
         baseUrl: ORS_SERVER
     });
@@ -15,4 +13,4 @@ export async function getRoute(profile: string, coords: [number, number][]){
         format: "geojson"
     })
     return route
-}
+}
