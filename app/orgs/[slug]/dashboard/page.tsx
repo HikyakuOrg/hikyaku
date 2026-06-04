a0a6ff2e47288ebc8d7d4dfe0169894a67a098a3
Daniel Quah <danielquah148@yahoo.com>
2026-06-04 16:06:50 +1000

    rename OUT_FOR_DELIVERY to IN_TRANSIT

--- app/orgs/[slug]/dashboard/page.tsx
+++ app/orgs/[slug]/dashboard/page.tsx
@@ -29,7 +29,7 @@ export default async function Page({ params }: { params: Promise<{ slug: string 
     recentPackagesData
   ] = await Promise.all([
     getPackagesCount(["PENDING"]),
-    getPackagesCount(["OUT_FOR_DELIVERY"]),
+    getPackagesCount(["IN_TRANSIT"]),
     getDriversCount(),
     getFleetSize(),
     getWarehousesCount(),
